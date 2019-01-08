668a5e6f1062056cf994c739010814eca8700323
christhopher.leao <christhopherleao@icloud.com>
2019-01-08 15:25:20 -0200

    added model schemas

--- api/model.js
+++ api/model.js
@@ -4,21 +4,19 @@ const shortid = require('shortid');
 
 mongoose.connect(process.env.DB_URL, { promiseLibrary: bluebird, useNewUrlParser: true });
 const schema = new mongoose.Schema({
-  _id: { type: String, default: shortid.generate },
+  id: { type: String, default: shortid.generate },
 
-  name: { type: String },
-  status: { type: String },
+  name: { type: String, trim: true },
+  email: { type: String, lowercase: true, trim: true, unique: true },
+  phone: { type: String, lowercase: true, trim: true, unique: true },
+  outro: { type: String },
+  contactby: { type: String },
+  assunto: {
+    type: String,
+    enum: ['cancelamento', 'outros', 'bagagem', 'atraso', 'overbooking'],
+    required: [true, 'É necessário fornecer o assunto']
+  }
 
-  rawFileUrl: { type: String },
-  rawFilePath: { type: String },
+}, { versionKey: false, timestamps: true, _id: false });
 
-  encoderId: { type: String },
-  encoderOutputs: [{
-    _id: { type: String },
-    url: { type: String },
-    format: { type: String },
-  }],
-
-}, { versionKey: false, timestamps: true });
-
-module.exports = mongoose.model('File', schema);
+module.exports = mongoose.model('Customer', schema);
