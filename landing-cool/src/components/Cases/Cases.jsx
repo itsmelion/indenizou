26ecfbe5d0eac423bc6ed629e481009480aebc18
christhopher.leao <christhopherleao@icloud.com>
2019-01-14 19:03:41 -0200

    added cases

--- landing-cool/src/components/Cases/Cases.jsx
+++ landing-cool/src/components/Cases/Cases.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
+import { yearsFromNow } from 'utilities';
+import cases from './testimonials-data';
 import './Cases.scss';
 
 const Cases = (
-  <section id="Cases">
-    {/* <h1>CASES</h1> */}
+  <section id="Cases" className="column contain" mobile-align="start center">
+    <h2 className="mb1">Casos e Depoimentos</h2>
+
+    <ul row="" align="around">
+      {cases.map(u => (
+        <li key={u.name} column="">
+          <h6 className="mb1">{u.assunto}</h6>
+          <article>{u.message}</article>
+          <span flex="" />
+          <legend className="mt1">
+            <h6>{u.name}, {yearsFromNow(u.birth_year)} anos</h6>
+            <sub>{u.address}</sub>
+          </legend>
+        </li>
+      ))}
+    </ul>
   </section>
 );
 
---
+++ landing-cool/src/components/Cases/testimonials-data.js
@@ -0,0 +1,44 @@
+export default [
+  {
+    assunto: 'Cancelamento',
+    name: 'Raoni',
+    message: `Meu vôo para Fortaleza foi cancelado e eu era padrinho de casamento.
+    Acabei perdendo o evento.
+    Me indicaram a Indenizou, que me deu todo suporte necessário e recebi
+    uma compensação financeira da compania pelo triste episódio.
+    Muito obrigado a vocês!`,
+    birth_year: 1981,
+    address: '',
+  },
+
+  {
+    assunto: 'Bagagem Danificada',
+    name: 'Lorena Fernandes',
+    message: `Minha bagagem foi totalmente danificada voltando de Buenos Aires.
+    Fiz minha reclamação no site da Indenizou e fui prontamente atendida e orientada.
+    Recebi uma indenização que amenizou todo transtorno causado!
+    Indiquei também duas amigas que tiveram voo cancelado!`,
+    birth_year: 1978,
+    address: '',
+  },
+
+  {
+    assunto: 'Atraso',
+    name: 'Liana Zebral',
+    message: `Meu voo atrasou muito e perdi um compromisso importantíssimo...
+    Fui muito bem acolhida pela Indenizou e recebi
+    minha indenização bem antes do que imaginava. Muito obrigada!!`,
+    birth_year: 1989,
+    address: '',
+  },
+
+  {
+    assunto: 'Atraso',
+    name: 'Luana de Paula',
+    message: `A Indenizou me orientou sobre todos os direitos
+    que eu tinha e me auxiliou na obtenção da minha justa indenização!
+    Parabéns a toda equipe envolvida!!`,
+    birth_year: 1964,
+    address: '',
+  },
+];
