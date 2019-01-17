ae5c2a782b686363e5307c72ccda7b773ad247d0
christhopher.leao <christhopherleao@icloud.com>
2019-01-17 20:08:49 -0200

    added common 'axios stuff' to layout

--- landing-cool/src/components/Layout.jsx
+++ landing-cool/src/components/Layout.jsx
@@ -2,8 +2,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
+import axios from 'axios';
+import axiosRetry from 'axios-retry';
 import { StaticQuery, graphql } from 'gatsby';
+import 'main.scss';
 // import Topbar from 'components/Topbar/Topbar';
+axiosRetry(axios, { retries: 3, retryDelay: axiosRetry.exponentialDelay });
 
 const Layout = ({ children }) => (
   <StaticQuery
--- landing-cool/src/pages/index.js
+++ landing-cool/src/pages/index.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import axiosRetry from 'axios-retry';
-import axios from 'axios';
 import Layout from 'components/Layout';
 import Footer from 'components/Footer/Footer';
 import Header from 'components/Header/Header';
@@ -8,8 +6,6 @@ import HowItWorks from 'components/HowItWorks/HowItWorks';
 import Pipeline from 'components/Pipeline/Pipeline';
 import Cases from 'components/Cases/Cases';
 import 'main.scss';
-
-axiosRetry(axios, { retries: 3, retryDelay: axiosRetry.exponentialDelay });
 
 const IndexPage = React.memo(() => (
   <Layout>
